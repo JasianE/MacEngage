c625baf9c3478a5bd0ab5525df270e5bb336cd14
jun li <junchenli423@gmail.com>
2026-02-07 15:29:00 -0500

    Added login and sign up

--- MacEngageFrontEnd/src/App.jsx
+++ MacEngageFrontEnd/src/App.jsx
@@ -2,13 +2,19 @@ import { Routes, Route } from "react-router-dom";
 
 // Pages
 import Home from "./pages/Home";
+import LoginPage from "./pages/LoginPage";
+import SignUp from "./pages/SignUp";
+import Landing from "./pages/LandingPage";
 
 function App() {
   //Fetch data and have a component that handles the various alert popups
   return (
     <>
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path='/' element = {<Landing/>} />
+      <Route path="/dashboard" element={<Home />} />
+      <Route path ="/login" element={<LoginPage />} />
+      <Route path ="/signup" element={<SignUp />} />
     </Routes>
     </>
   );
